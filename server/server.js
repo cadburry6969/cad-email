407d7d09f6a364f4655611e32eb7452a4c551fe4
cadburry <72443203+cadburry6969@users.noreply.github.com>
2021-12-31 22:56:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,21 +4,21 @@
 // -- / Dependency and config \ -- \\
 const { Client, Intents } = require('discord.js');
 const mailbot = new Client({ intents: [Intents.FLAGS.GUILDS] });
-global.config = require("./config.json")
+const config = require("./config.json")
 
 // -- / On Bot ready Do \ -- \\
-mailbot.on('ready', () => {        
+mailbot.once("ready", () => { // prints "Ready!" to the console once the bot is online
     console.log("\nMailBot (FiveM to Discord DM). Made By Cadburry#7547.\n");         
     console.log(`Discord Bot logged in as ${mailbot.user.tag}`);    
-    mailbot.user.setStatus('dnd');     
-    updateStatus(mailbot, 10) // updating every 10 secs        
+    mailbot.user.setStatus('dnd').catch(err => {return console.log(err)});     
+    updateStatus(mailbot, 30) // updating every 30 secs   
 });
 
 // -- / Functions \ -- \\
 function updateStatus(client, seconds) { // Adds Bot Activity
    setInterval(function() {        
-        let status = `${GetNumPlayerIndices()} Players`            
-        client.user.setActivity(status, {type: 'WATCHING'})
+        const status = `${GetNumPlayerIndices()} Players`            
+        client.user.setActivity(status, {type: 'WATCHING'}).catch(err => {return console.log(err)})
     }, seconds * 1000)    
 }
 
@@ -35,27 +35,20 @@ var QBCore = global.exports['qb-core'].GetCoreObject();
 
 // -- / Event which send request to bot \ -- \\
 onNet("cademailSendMailServer", async (name, discord, subject, body, email) => {        
-    let ts = Date.now();
-    let date_ob = new Date(ts);     
-    let DateTime = `${IntTwoChars(date_ob.getHours())}:${IntTwoChars(date_ob.getMinutes())}:${IntTwoChars(date_ob.getSeconds())} ${date_ob.getDate()}/${date_ob.getMonth() + 1}/${date_ob.getFullYear()}`;    
+    const date = new Date();    
 	mailbot.users.fetch(discord).then((user) => 
-        user.send('> **From:** '+email+' \n> \n> **Subject:** '+subject+' \n> **Body:** '+body+' \n> \n> **Sign:** '+name+' \n> \n> **Recieved on:** '+DateTime).catch(err => {
-            let error = emitNet("cademailSendNotify", src, "Cannot send message (DM's Off)")
+        user.send('> **From:** '+email+' \n> \n> **Subject:** '+subject+' \n> **Body:** '+body+' \n> \n> **Sign:** '+name+' \n> \n> '+date).catch(err => {
+            const error = emitNet("cademailSendNotify", src, "Cannot send message (DM's Off)")
             return error
         })
     );    
 });       
 
 onNet("cademailSendMailinfo", async (data) => {        
-    let src = source
-    let Player = QBCore.Functions.GetPlayer(src)
-    let name = Player.PlayerData.charinfo.firstname+" "+Player.PlayerData.charinfo.lastname
-    let email = Player.PlayerData.charinfo.firstname+"_"+Player.PlayerData.charinfo.lastname+"@email.com"
-    let discord = data['primary'][0]
-    let subject = data['primary'][1]
-    let description = data['primary'][2]
-    emitNet("cademailMailSent", src, name, discord, subject, description, email)
-});       
+    const src = source
+    const { firstname, lastname } = QBCore.Functions.GetPlayer(src).PlayerData.charinfo;
+    emitNet("cademailMailSent", src, `${firstname} ${lastname}`, data['primary'][0], data['primary'][1], data['primary'][2], `${firstname}_${lastname}@email.com`);
+});     
 
 // -- / COMMANDS \ -- \\
 QBCore.Commands.Add('email', 'Send Email to Someone', {}, false, (source, args)  => {
